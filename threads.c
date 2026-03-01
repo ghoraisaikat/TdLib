4edc8b4a62bf05613a2f3ce1290f44b69dad711d
Saikat Ghorai <beth@Saikats-MacBook-Air.local>
2026-03-01 11:48:42 +0530

    Add preemptive timer

--- threads.c
+++ threads.c
@@ -1,53 +1,75 @@
 #define _XOPEN_SOURCE 600
+#include <sys/time.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <ucontext.h>
+#include <string.h>
 
 #define endl "\n"
 #define sp " "
+#define debug() printf("I reached here!" endl)
 
 
-ucontext_t ctx_f1, ctx_f2, ctx_main;
+const int TASKS = 2;
+int curr_TASK = 0;
+ucontext_t tasks[TASKS];
+ucontext_t ctx_main;
+
+void handleAlarm(int signum) {
+	int curr_id = curr_TASK;
+	int curr_next = (curr_id + 1) % 2;
+	curr_TASK = curr_next;
+	printf("Context switch baby!" endl);
+	swapcontext(&tasks[curr_id], &tasks[curr_next]);
+}
 
 void func1(void) {
-
+	while(1){
 		printf("Bing!" endl);
-		usleep(100000);
-		swapcontext(&ctx_f1, &ctx_f2);
-		swapcontext(&ctx_f1, &ctx_main);	
+		usleep(75000);
+	}
 }
 
 void func2(void) {
-	
+	while(1){
 		printf("Bong!" endl);
-		usleep(100000);
-		swapcontext(&ctx_f2, &ctx_f1);
+		usleep(75000);
+	}
 }
 
 int main(void){
+	signal(SIGALRM, handleAlarm);
 	if (getcontext(&ctx_main) == -1) 
 		perror("main, getcontext");
-	
+
 	void* STACK_f1 = malloc(4096);
 	void* STACK_f2 = malloc(4096);
-	if (getcontext(&ctx_f1) == -1) 
+	if (getcontext(&tasks[0]) == -1) 
 		perror("f1, getcontext");
-	if (getcontext(&ctx_f2) == -1)
+	if (getcontext(&tasks[1]) == -1)
 		perror("f2, getcontext");
+	tasks[0].uc_stack.ss_sp 	= STACK_f1;
+	tasks[0].uc_stack.ss_size 	= 4096;
+	tasks[0].uc_link 		= NULL;
 
-	ctx_f1.uc_stack.ss_sp 	= STACK_f1;
-	ctx_f1.uc_stack.ss_size = 4096;
-	ctx_f1.uc_link 		= NULL;
+	tasks[1].uc_stack.ss_sp		= STACK_f2;
+	tasks[1].uc_stack.ss_size 	= 4096;
+	tasks[1].uc_link		= NULL;
+	
+	makecontext(&tasks[0], (void (*)(void)) func1, 0);
+	makecontext(&tasks[1], (void (*)(void)) func2, 0);
+	
 
-	ctx_f2.uc_stack.ss_sp	= STACK_f2;
-	ctx_f2.uc_stack.ss_size = 4096;
-	ctx_f2.uc_link		= NULL;
-
-	makecontext(&ctx_f1, (void (*)(void)) func1, 0);
-	makecontext(&ctx_f2, (void (*)(void)) func2, 0);
-
-	swapcontext(&ctx_main, &ctx_f1);
+	struct itimerval timer;
+	timer.it_interval.tv_usec	= 150000;
+	timer.it_interval.tv_sec	= 0;
+	timer.it_value.tv_usec		= 150000;
+	timer.it_value.tv_sec		= 0;
+	
+	if (setitimer(ITIMER_REAL, &timer, NULL) == -1)
+		perror("setitimer");
+	swapcontext(&ctx_main, &tasks[0]);
 	printf("Came back from a long day's work!" endl);
 	return 0;
 }
